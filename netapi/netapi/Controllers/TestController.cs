0d67264440c24406d599a4549f498f500054c9cf
user1 <691784758@qq.com>
2022-05-07 16:46:06 +0800

    test

---
+++ netapi/netapi/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Route("Get")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = "success";
+            return new JsonResult(result);
+        }
+    }
+}
